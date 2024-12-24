5a545ca16c74b63823a0ebbb1690816fd7fd8b10
Akanksha0000 <akankshaubale68@gmail.com>
2024-12-24 12:11:14 +0530

    add files in user details screen

--- lib/main.dart
+++ lib/main.dart
@@ -32,8 +32,7 @@ class MyApp extends StatelessWidget {
             "created_at": "2024-12-13T07:31:48.901818Z",
             "updated_at": "2024-12-13T07:31:48.901831Z"
           },
-          accessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzM0NjEyNjYwLCJpYXQiOjE3MzQ1OTEwNjAsImp0aSI6IjI0NzVjMGU3MmE1YTQ3ZTFhMTI5N2IyNjJiMDQ4YzRiIiwidXNlcl9pZCI6IjdlNWJmMDU3LWZlYjEtNGU4ZC1iZTI2LTcwN2Y3ZGNlNmQ2OSJ9.yddrwVNX9Q3r9ba1BH8gvo24gVR28lAwBqemfRDb_is",
-           refreshToken: '',
+         accessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzM1MDgxMzM5LCJpYXQiOjE3MzUwMTc3MzksImp0aSI6ImFkNTUzZDhhYjYzYjQ1NDg4NWZlZTQ2YjZkZDQ2MTE4IiwidXNlcl9pZCI6ImJjNTRhMzdjLWEyNzAtNGJmYi1iZGIwLTJiODRjMTczYTkxNiJ9.N6fIXcN44_RS6YAcAqZ7fufTymMCP3BuZn-pP5-YtIA",           refreshToken: '',
          // refreshToken:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzM0NTA0NzQ4LCJpYXQiOjE3MzQ1MDExNDgsImp0aSI6IjBlYTJkY2U1Yzk1ZDQ0ZWU4MzNjZWQ2NTA4ODE4NDUyIiwidXNlcl9pZCI6IjdlNWJmMDU3LWZlYjEtNGU4ZC1iZTI2LTcwN2Y3ZGNlNmQ2OSJ9.lcDga5B6CrDgUJOMki5KjTY55fB7dfhpBnJHBNlChmg",
         ),
       ),
